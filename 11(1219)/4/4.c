1b17b4432127beed6d33a51f6841dc874851ac71
kaito <4681kaito@gmail.com>
2023-12-22 14:32:19 +0900

    1219課題最終

--- 11(1219)/4/4.c
+++ 11(1219)/4/4.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fibonacci(int n, int f[41]);
+int fibonacci(int n, int *f);
 
 int main(int argc, char *argv[]) {
     int n, i, f[41];
@@ -27,120 +27,84 @@ int main(int argc, char *argv[]) {
     return -1;
 }
 
+int fibonacci(int n, int *f) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else if (f[n] != -1) {
+        printf("Call: Fibonacci(%d)\n", n);
+    } else {
+        printf("Call: Fibonacci(%d)\n", n);
+        if (f[n - 1] == -1 && f[n - 2] == -1) {
+            f[n - 1] = fibonacci(n - 1, f);
+            if (n - 1 == 0) {
+                printf("Call: Fibonacci(0)\n");
+                printf("Fibonacci(0) = 0\n");
+            } else if (n - 1 == 1) {
+                printf("Call: Fibonacci(1)\n");
+                printf("Fibonacci(1) = 1\n");
+            }
 
+            f[n - 2] = fibonacci(n - 2, f);
+            if (n - 2 == 0) {
+                printf("Call: Fibonacci(0)\n");
+                printf("Fibonacci(0) = 0\n");
+            } else if (n - 2 == 1) {
+                printf("Call: Fibonacci(1)\n");
+                printf("Fibonacci(1) = 1\n");
+            }
 
+        } else if (f[n - 1] == -1) {
+            f[n - 1] = fibonacci(n - 1, f);
+            if (n - 1 == 0) {
+                printf("Call: Fibonacci(0)\n");
+                printf("Fibonacci(0) = 0\n");
+            } else if (n - 1 == 1) {
+                printf("Call: Fibonacci(1)\n");
+                printf("Fibonacci(1) = 1\n");
+            }
+
+        } else if (f[n - 2] == -1) {
+            f[n - 2] = fibonacci(n - 2, f);
+            if (n - 2 == 0) {
+                printf("Call: Fibonacci(0)\n");
+                printf("Fibonacci(0) = 0\n");
+            } else if (n - 2 == 1) {
+                printf("Call: Fibonacci(1)\n");
+                printf("Fibonacci(1) = 1\n");
+            }
+        }
+
+        f[n] = f[n - 1] + f[n - 2];
+        printf("Fibonacci(%d) = %d\n", n, f[n]);
+    }
+    return f[n];
+}
 
 // int fibonacci(int n, int *f) {
 //     if (n == 0) {
+// 		printf("Call: Fibonacci(%d)\n", n);
 //         return 0;
 //     } else if (n == 1) {
+// 		printf("Call: Fibonacci(%d)\n", n);
 //         return 1;
 //     } else if (f[n] != -1) {
-// 		printf("%d", n);
-//         //
+//         printf("     Call: Fibonacci(%d)\n", n);
 //     } else {
-// 		printf("Call: Fibonacci(%d)\n",n);
-//         if (f[n - 1] == -1) {
+// 		printf("Call: Fibonacci(%d)\n", n);
+//         if (f[n - 1] == -1 && f[n - 2] == -1) {
 //             f[n - 1] = fibonacci(n - 1, f);
-// 			if(n == 1 || n == 2){
-// 				int fibonacci(int n, int *f) {
-//     if (n == 0) {
-//         return 0;
-//     } else if (n == 1) {
-//         return 1;
-//     } else if (f[n] != -1) {
-// 		printf("%d", n);
-//         //
-//     } else {
-// 		printf("Call: Fibonacci(%d)\n",n);
-//         if (f[n - 1] == -1) {
+// 			f[n - 2] = fibonacci(n - 2, f);
+//         } else if (f[n - 1] == -1 ) {
 //             f[n - 1] = fibonacci(n - 1, f);
-// 			if(n == 1 || n == 2){
-// 				printf("Call: Fibonacci(%d)\n",n-1);
-// 			}
-//             printf("Fibonacci(%d) = %d\n", n - 1, f[n - 1]);
-//         }
-
-//         if (f[n - 2] == -1) {
-//             f[n - 2] = fibonacci(n - 2, f);
-// 			if(n == 2 || n == 3){
-// 				printf("Call: Fibonacci(%d)\n",n-2);
-// 			}
-//             printf("Fibonacci(%d) = %d\n", n - 2, f[n - 2]);
-//         }
-//         f[n] = f[n - 1] + f[n - 2];
-//         printf("Fibonacci(%d) = %d\n", n, f[n]);
-//     }
-//     return f[n];
-// }
-// 			}
-//             printf("Fibonacci(%d) = %d\n", n - 1, f[n - 1]);
-//         }
-
-//         if (f[n - 2] == -1) {
-//             f[n - 2] = fibonacci(n - 2, f);
-// 			if(n == 2 || n == 3){
-// 				printf("Call: Fibonacci(%d)\n",n-2);
-// 			}
-//             printf("Fibonacci(%d) = %d\n", n - 2, f[n - 2]);
-//         }
-//         f[n] = f[n - 1] + f[n - 2];
-//         printf("Fibonacci(%d) = %d\n", n, f[n]);
-//     }
-//     return f[n];
-// }
-// int fibonacci(int n, int f[41]) {
-//     if (n == 0) {
-//         // printf("Call: Fibonacci(0)\n");
-// 		// f[0] = 0;
-//         return 0;
-//     } else if (n == 1) {
-//         // printf("Call: Fibonacci(1)\n");
-// 		// f[1] = 1;
-//         return 1;
-//     } else if (f[n] != -1) {
-//         printf("Call: Fibonacci(%d)\n", n);
-//     } else {
-//         printf("Call: Fibonacci(%d)\n", n);
-
-//         f[n - 1] = fibonacci(n - 1, f);
-//         f[n - 2] = fibonacci(n - 2, f);
-//         f[n] = f[n - 1] + f[n - 2];
-//     }
-
-//     printf("Fibonacci(%d) = %d\n", n, f[n]);
-//     return f[n];
-// }
-
-// int fibonacci(int n, int f[41]) {
-//     if (n == 0) {
-//         return 0;
-//     } else if (n == 1) {
-//         return 1;
-//     } else if (f[n] != -1) {
-//         printf("Call: Fibonacci(%d)\n", n);
-//     } else {
-//         printf("Call: Fibonacci(%d)\n", n);
-
-//         if ((n == 2 || n == 1) && f[n - 1] == -1) {
-//             printf("Call: Fibonacci(%d)\n", n - 1);
-//             f[n - 1] = fibonacci(n - 1, f);
-//             printf("Fibonacci(%d) = %d\n", n - 1, f[n - 1]);
-//         } else {
-//             f[n - 1] = fibonacci(n - 1, f);
-//         }
-
-//         if ((n == 3 || n == 2) && f[n - 2] == -1) {
-//             printf("Call: Fibonacci(%d)\n", n - 2);
-//             f[n - 2] = fibonacci(n - 2, f);
-//             printf("Fibonacci(%d) = %d\n", n - 2, f[n - 2]);
-//         } else {
+//         } else if (f[n - 2] == -1 ) {
 //             f[n - 2] = fibonacci(n - 2, f);
 //         }
 
-//         f[n] = f[n - 1] + f[n - 2];
-//     }
+// 		f[n] = f[n-1] + f[n-2];
+// 		printf("\nFibonacci(%d) = %d\n", n, f[n]);
 
-//     printf("Fibonacci(%d) = %d\n", n, f[n]);
+//     }
 //     return f[n];
 // }
